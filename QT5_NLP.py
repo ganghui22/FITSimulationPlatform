9b56362c5bbc0e6894f1757ff03185f3c4f2a208
ganghui22 <ganghui22@163.com>
2022-07-21 15:44:33 +0800

    添加了地图标注的功能

--- QT5_NLP.py
+++ QT5_NLP.py
@@ -1,10 +1,12 @@
 import json
 import multiprocessing
+import os
 import threading
 
 import cv2
 import time
 import random
+import numpy as np
 from multiprocessing import Process, Queue
 from PyQt5.QtCore import QTimer, QSize, QDateTime, Qt, QPoint, QThread, pyqtSignal
 from PyQt5.QtWidgets import *
@@ -17,6 +19,7 @@ from QtCustomComponents.qnchatmessage import QNChatMessage
 from PathPlanningAstar.util_llj.AStar import *
 from update_time_space_graph import deal
 import itertools
+import pickle
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -40,6 +43,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # 加载地图
         self._map = QPixmap("PathPlanningAstar/map.png")
+        self._map_cv2 = cv2.imread("PathPlanningAstar/map.png")
         self._map_w, self._map_h = self._map.width(), self._map.height()
 
         # 加载机器人头像
@@ -48,14 +52,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # 设置全屏显示
         self.setGeometry(self.centralwidget.x() + 0, self.centralwidget.height() + 0, desktop_w, desktop_h)
-        self.centralwidget.setGeometry(0,
-                                       0,
-                                       self.width(),
-                                       self.height() - self.menubar.height() - self.statusbar.height())
+        self.centralwidget.setGeometry(0, 0,
+                                       self.width(), self.height() - self.menubar.height() - self.statusbar.height())
 
         # map scene 设置, 长宽为map的分辨率
-        self.map_scene = QGraphicsScene(0, 0, self._map_w,
-                                        self._map_h)
+        self.map_scene = QGraphicsScene(0, 0, self._map_w, self._map_h)
         # 向map scene中添加地图
         self.map_scene_map_item = self.map_scene.addPixmap(self._map)
         # 向map scene中添加机器人
@@ -134,7 +135,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # 加载地点列表
         with open('data/Location_list.json', 'r', encoding='utf-8') as f:
-            self._location_list = json.load(f)
+            self._location_list: dict = json.load(f)
 
         # 生成不同颜色的未知头像
         unk_head = cv2.imread("ProfilePicture/Unk.png", cv2.IMREAD_UNCHANGED)
@@ -164,6 +165,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # 向statusbar添加map_view_real的信息打印
         self.map_view_real_status = QLabel()
+        self.map_view_real_status.setMinimumWidth(150)
         self.statusbar.addWidget(self.map_view_real_status)
 
         # 重写map_view_real 的mouseMoveEvent函数
@@ -209,6 +211,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.__waitForPathPlanning = False
         self.__moveTimer.start(self.__moveSpeed)
 
+        # 画框标志位
+        self.isDragRect = False
+        self.dragStartPt = QPoint()
+        self.dragRectPressFlag = False
+
         # 列表扫描服务
         if dialogue_list is not None:
             self.dialogue_list = dialogue_list
@@ -217,56 +224,240 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.dialogue_list_deal_timer.start(2000)
 
         self.actionTag_location.setCheckable(True)
+        self.actionTag_location.triggered.connect(self._Tag_location_fun)
         self.action_exit.triggered.connect(self.action_exit_fun)
 
+        # 初始化标注的房间item列表
+        self.room_item_list = []
+        self.map_scene_room_item_dict = {}
+        self._load_map_room_item()
+
+        # tag location槽函数连接
+        self.map_view_real.mouseReleaseEvent = self._map_view_real_ReleaseEvent
+        self.map_view_real.mousePressEvent = self._map_view_real_PressEvent
+
+        # save TagLocation槽函数连接
+        self.actionsave_Tag_Location = self.save_map_scene_room_item_dict
+
+    def keyPressEvent(self, a0: QKeyEvent) -> None:
+        """
+        重写按键Press事件
+        """
+        if a0.key() == Qt.Key.Key_Escape:
+            self.close()
+        if a0.key() == Qt.Key.Key_Delete:
+            QGraphicsItemList = self.map_scene.selectedItems()
+            if QGraphicsItemList:
+                for item in QGraphicsItemList:
+                    if isinstance(item, QGraphicsPixmapItem):
+                        name = item.data(0)
+                        self.map_scene.removeItem(item)
+                        self.map_scene.removeItem(self.map_scene_room_item_dict[name]["name_label"])
+                        self.map_scene_room_item_dict.pop(name)
+                        self.actionTag_location.setVisible(True)
+
+        return QMainWindow.keyPressEvent(self, a0)
+
+    def save_map_scene_room_item_dict(self):
+        """
+        room信息保存功能的实现
+        """
+        d = {}
+        for name in self.map_scene_room_item_dict:
+            d[name] = {}
+            d[name]['argument'] = {}
+            d[name]['argument']['color'] =self.map_scene_room_item_dict[name]["argument"]["color"]
+            self.map_scene_room_item_dict[name]["argument"]["label_pos"] = \
+                [int(self.map_scene_room_item_dict['name_label'].x()),
+                 int(self.map_scene_room_item_dict['name_label'].y())]
+            room_name_label_pos = self.map_scene_room_item_dict[name]["argument"]["label_pos"]
+            d[name][name]["argument"]["label_pos"] = room_name_label_pos
+            d[name]['argument']['name'] = self.map_scene_room_item_dict[name]['argument']["name"]
+            d[name]['argument']['rect'] = self.map_scene_room_item_dict[name]['argument']["rect"]
+            d[name]['argument']['room'] = self.map_scene_room_item_dict[name]['argument']["room"]
+        with open("data/fit4_5Dealing.pkl", 'wb') as f:
+            pickle.dump(d, f)
+
+    def _load_map_room_item(self):
+        """
+        加载保存的房间标注信息
+        """
+        if os.path.exists("data/fit4_5Dealing.pkl"):
+            with open("data/fit4_5Dealing.pkl", 'rb') as f:
+                d = pickle.load(f)
+            for r in d:
+                # 加载room信息
+                rect = d[r]['argument']['rect']
+                room = d[r]['argument']['room']
+                room_color = d[r]['argument']['color']
+                color = (255 - room_color).tolist() + [200]
+                room_name_label_pos = d[r]['argument']['label_pos']
+
+                name = d[r]['argument']['name']
+                # room标签设置
+                room_name_label: QGraphicsTextItem = self.map_scene.addText(name)
+                room_name_label.setDefaultTextColor(QColor(color[0], color[1], color[2], color[3]))
+                font = QFont("Times", 36)
+                room_name_label.setFont(font)
+                room_name_label.setPos(room_name_label_pos[0], room_name_label_pos[1])
+                room_name_label.setFlags(QGraphicsItem.ItemIsSelectable |
+                                         QGraphicsItem.ItemIsMovable |
+                                         QGraphicsItem.ItemIsFocusable)
+                room_name_label.setZValue(4)
+
+                Q_room_Image = QImage(room.data, room.shape[1], room.shape[0], room.shape[1] * 4,
+                                      QImage.Format_RGBA8888)
+                Q_room = QPixmap().fromImage(Q_room_Image)
+                map_room_item: QGraphicsPixmapItem = self.map_scene.addPixmap(Q_room)
+                map_room_item.setPos(rect[0], rect[1])
+                map_room_item.setZValue(1)
+                map_room_item.setShapeMode(QGraphicsPixmapItem.MaskShape)
+                map_room_item.setFlags(QGraphicsItem.ItemIsSelectable |
+                                       QGraphicsItem.ItemIsFocusable |
+                                       QGraphicsItem.ItemClipsToShape
+                                       )
+                map_room_item.setData(0, name)  # 自定义map_room_item的值
+                map_room_item.setCacheMode(QGraphicsItem.DeviceCoordinateCache)
+                self.map_scene_room_item_dict[r] = d[r]
+                self.map_scene_room_item_dict[r]['item'] = map_room_item
+                self.map_scene_room_item_dict[r]['name_label'] = room_name_label
+            items = []
+            for room_name in self._location_list:
+                if room_name not in self.map_scene_room_item_dict:
+                    items.append(room_name)
+            if items:
+                pass
+            else:
+                self.actionTag_location.setCheckable(False)
+                self.actionTag_location.setVisible(False)
+
     def action_exit_fun(self):
+        """
+        程序退出时调用的函数
+        """
         if QMessageBox(QMessageBox.Icon.Question, "确认退出", "是否退应用程序？",
                        QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No) \
                 .exec_() == QMessageBox.StandardButton.Yes:
             self.close()
 
+    def _map_view_real_ReleaseEvent(self, e: QMouseEvent):
+        """
+        重写 map_view_real的鼠标释放事件。
+        释放时若是DragRec模式读取 RubberBandDrag所选中区域进行区域内房间选择，该功能待优化，效果不是太好。
+        """
+        print(self.actionTag_location.isChecked())
+        if self.dragRectPressFlag & self.actionTag_location.isChecked():
+            # 获取roi 区域
+            r = self.map_view_real.mapToScene(self.map_view_real.rubberBandRect())  # [0左上角，1右上角，2右下角，3左下角]
+
+            # 原图mask
+            mask = np.zeros(self._map_cv2.shape[:2], dtype=np.uint8)
+
+            # 矩形roi
+            rect = (int(r[0].x()), int(r[0].y()), int(r[2].x() - r[0].x()), int(r[2].y() - r[0].y()))
+            bgdmodel = np.zeros((1, 65), np.float64)  # bg模型的临时数组
+            fgdmodel = np.zeros((1, 65), np.float64)  # fg模型的临时数组
+
+            # 提取区域
+            cv2.grabCut(self._map_cv2, mask, rect, bgdmodel, fgdmodel, 3, mode=cv2.GC_INIT_WITH_RECT)
+
+            # 提取前景和可能的前景区域
+            room_mask = np.array(mask[int(r[0].y()):int(r[2].y()),
+                                 int(r[0].x()):int(r[2].x())], dtype='uint8')
+            room_mask = np.where((room_mask == 1) + (room_mask == 3),
+                                 125, 0).reshape(room_mask.shape[0], room_mask.shape[1], 1)
+            room_color = np.random.randint(0, 255, 3)
+            room = room_color * np.ones((room_mask.shape[0], room_mask.shape[1], 1), np.uint8)
+            room = np.concatenate((room, room_mask), axis=2).astype(np.uint8)
+            Q_room_Image = QImage(room.data, room.shape[1], room.shape[0], room.shape[1] * 4,
+                                  QImage.Format_RGBA8888)
+            Q_room = QPixmap().fromImage(Q_room_Image)
+
+            # 记录当前房间item
+            map_room_item: QGraphicsPixmapItem = self.map_scene.addPixmap(Q_room)
+            map_room_item.setPos(r[0])
+            map_room_item.setZValue(1)
+            map_room_item.setShapeMode(QGraphicsPixmapItem.MaskShape)
+            map_room_item.setFlags(QGraphicsItem.ItemIsSelectable |
+                                   QGraphicsItem.ItemIsFocusable |
+                                   QGraphicsItem.ItemClipsToShape)
+
+            items = []
+            for room_name in self._location_list:
+                if room_name not in self.map_scene_room_item_dict:
+                    items.append(room_name)
+            name, boolAction = QInputDialog.getItem(self.window(), "指定房间", "房间:", items, 0, False)
+            if boolAction and name != '':
+                self.map_scene_room_item_dict[name] = {}
+                room_name_label: QGraphicsTextItem = self.map_scene.addText(name)
+                color = (255 - room_color).tolist() + [200]
+                room_name_label.setDefaultTextColor(QColor(color[0], color[1], color[2], color[3]))
+                font = QFont("Times", 28)
+                room_name_label.setFont(font)
+                room_name_label.setFlags(QGraphicsItem.ItemIsSelectable |
+                                         QGraphicsItem.ItemIsFocusable)
+                room_name_label.setZValue(4)
+                room_name_label_pos = [int(rect[0] + rect[2] / 2), int(rect[1] + rect[3] / 2 - 36)]
+                room_name_label.setPos(room_name_label_pos[0], room_name_label_pos[1])
+                map_room_item.setData(0, name)  # 自定义map_room_item的值
+                self.map_scene_room_item_dict[name]["item"] = map_room_item
+                self.map_scene_room_item_dict[name]["name_label"] = room_name_label
+                self.map_scene_room_item_dict[name]["argument"] = {}
+                self.map_scene_room_item_dict[name]["argument"]["color"] = room_color
+                self.map_scene_room_item_dict[name]["argument"]["label_pos"] = room_name_label_pos
+                self.map_scene_room_item_dict[name]["argument"]["name"] = name
+                self.map_scene_room_item_dict[name]["argument"]["rect"] = rect
+                self.map_scene_room_item_dict[name]["argument"]["room"] = room
+                if len(items) == 1 and name == items[0]:  # 最后一个房间已被选择完成，将该模式隐藏
+                    self.map_view_real.setDragMode(QGraphicsView.NoDrag)
+                    self.map_view_real.setCursor(Qt.ArrowCursor)
+                    self.actionTag_location.setCheckable(False)
+                    self.actionTag_location.setDisabled(True)
+            else:
+                self.map_scene.removeItem(map_room_item)
+            # drag模式选择Flag
+            self.dragRectPressFlag = False
+        return QGraphicsView.mouseReleaseEvent(self.map_view_real, e)
+
     def _map_view_real_mouseMoveEvent(self, event: QMouseEvent):
+        """
+        重写map_view_real的鼠标移动事件，打印鼠标指向的地图像素点
+        """
         view_pos = event.pos()
         scene_pos = self.map_view_real.mapToScene(view_pos)
         self.window().map_view_real_status.setText("<font color='red'>X</font>:<font color='blue'>{}</font>, "
                                                    "<font color='red'>Y</font>:<font color='blue'>{}</font>"
                                                    .format(scene_pos.x(), scene_pos.y()))
+        return QGraphicsView.mouseMoveEvent(self.map_view_real, event)
 
-    def _Tag_location(self):
-        self.window().setCursor(Qt.CrossCursor)
-        self.map_view.mouseMoveEvent = self._Tag_action_location_mouseMoveEvent
+    def _map_view_real_PressEvent(self, e: QMouseEvent):
+        """
+        重写map_view_real的鼠标按压事件，若是dragRect的模式则置位dragRectPressFlag标记位
+        """
+        if self.actionTag_location.isChecked():
+            self.dragRectPressFlag = True
+        return QGraphicsView.mousePressEvent(self.map_view_real, e)
+
+    def _Tag_location_fun(self, check: bool):
+        """
+        Tag_location动作调用的函数
+        """
+        if check:
+            self.map_view_real.setDragMode(QGraphicsView.RubberBandDrag)
+            self.map_view_real.setCursor(Qt.CrossCursor)
+        else:
+            self.map_view_real.setCursor(Qt.ArrowCursor)
+            self.map_view_real.setDragMode(QGraphicsView.NoDrag)
 
     def _dialogue_list_deal(self):
+        """
+        对话列表的处理函数
+        """
         if self.dialogue_list:
             dialogue = self.dialogue_list.pop(0)
             self.dealMessage(dialogue)
         else:
             self.dialogue_list_deal_timer.stop()
-
-    def _Tag_action_location_mousePressEvent(self, event: QMouseEvent):
-        """
-        位置标注动作触发时的鼠标按压事件
-        """
-        if event.button() == Qt.LeftButton:
-            self.window().map.start_pos = event.pos()
-            self.map.mouseDown = True
-
-    def _Tag_action_location_mouseMoveEvent(self, event: QMouseEvent):
-        """
-        位置标注动作触发时的鼠标移动事件
-        """
-        if event.button() == Qt.LeftButton:
-            if self.window().map.mouseDown:
-                self.window().map.end_pos = event.pos()
-                self.paint()
-
-    def _Tag_action_location_mouseReleaseEvent(self, event: QMouseEvent):
-        """
-        位置标注动作触发时的鼠标释放事件
-        """
-        if self.window().map.mouseDown:
-            self.window().map.end_pos = event.pos()
-            self.self.window().map.mouseDown = False
 
     def __dealMessageShow(self, messageW: QNChatMessage, item: QListWidgetItem,
                           text: str, name: str, time: int, usertype: QNChatMessage.User_Type):
@@ -278,11 +469,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         item.setSizeHint(size)
         messageW.setText(text, time, name, size, usertype)
         self.listWidget.setItemWidget(item, messageW)
-
-    def map_mouseMoveEvent(self, event: QMouseEvent):
-        x, y = event.pos().x(), event.pos().y()
-        self.window().statuLabel.setText("<font color='red'>X</font>:<font color='blue'>{}</font>, "
-                                         "<font color='red'>Y</font>:<font color='blue'>{}</font>".format(x, y))
 
     def __dealMessageTime(self, curMsgTime: int):
         """
@@ -370,6 +556,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                    logText + "\n")
 
     def __userChanged(self, signal, user: str = None):
+        """
+        用户改变的槽函数
+        :param user: 当传入user为None时，则选取UserComboBox当前指定的用户
+        :return:
+        """
         if user is None:
             currentUser = self.UserComboBox.currentText()
             for key in self._Person:
@@ -613,12 +804,12 @@ def InstructionPrediction_process(qi: multiprocessing.Queue, qo: multiprocessing
 
 def main():
     dialogue_list = None
-    dialogue_list = ['刘老师:该赶我们的进度了，下午2点钟在1号会议室碰一下怎么样？',
-                     '港晖:抱歉，老师，我参加不了，我下午2点要在Room511开个线上的会议',
-                     '刘老师:好，其他人呢？',
-                     '兰军:我准时到',
-                     '小飞:我没问题'
-                     ]
+    # dialogue_list = ['刘老师:该赶我们的进度了，下午2点钟在1号会议室碰一下怎么样？',
+    #                  '港晖:抱歉，老师，我参加不了，我下午2点要在Room511开个线上的会议',
+    #                  '刘老师:好，其他人呢？',
+    #                  '兰军:我准时到',
+    #                  '小飞:我没问题'
+    #                  ]
 
     # 指令解析服务的进程通信队列
     InstructionPrediction_InQueue, InstructionPrediction_OutQueue = multiprocessing.Queue(), multiprocessing.Queue()
